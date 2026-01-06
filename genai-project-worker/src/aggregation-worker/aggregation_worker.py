b99db41e16d37b16f08f839c69375e99781bbf95
Domonkos Máté Tóth <tothdodo01@gmail.com>
2026-01-06 16:25:06 +0100

    Workers initial setup

---
+++ genai-project-worker/src/aggregation-worker/aggregation_worker.py
@@ -0,0 +1,10 @@
+import logging
+
+
+def main():
+    # logging.info(f"Connected to RabbitMQ Listening on queue '{rabbitConfig.queue_comparison_job}'")
+    logging.info("Aggregation Worker Started!")
+
+
+if __name__ == '__main__':
+    main()
