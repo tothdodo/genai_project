fd7a20545f90c79bbd3def49860b924a6fcf9118
ruszkamuszka <ruszkamartin@gmail.com>
2026-01-07 19:49:24 +0100

    backend setup

---
+++ genai-project-backend/src/main/java/genai/genaiprojectbackend/GenaiProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package genai.genaiprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenaiProjectBackendApplication.class, args);
+    }
+
+}
