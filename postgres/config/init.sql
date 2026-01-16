48d1d80b7436df751995b138cfd40de60e03c28d
Domonkos Máté Tóth <tothdodo01@gmail.com>
2026-01-16 12:44:50 +0100

    Init.sql update

--- postgres/config/init.sql
+++ postgres/config/init.sql
@@ -35,71 +35,80 @@ CREATE TABLE IF NOT EXISTS category_items (
     );
 
 CREATE TABLE IF NOT EXISTS files (
-     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-     filename TEXT       NOT NULL UNIQUE,
-     original_filename   TEXT,
-     size_bytes BIGINT,
-     file_creation_date  DATE,
-     uploaded            BOOLEAN DEFAULT FALSE,
-     uploaded_at         TIMESTAMP,
-     category_item_id    INTEGER,
-
-     CONSTRAINT fk_category_item_id
-     FOREIGN KEY (category_item_id) REFERENCES category_items(id) ON DELETE SET NULL
-    );
-
-CREATE TABLE text_chunks (
-     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-     file_id             INTEGER NOT NULL,
-     chunk_index         INTEGER NOT NULL,
-     page_start          INTEGER,
-     page_end            INTEGER,
-     text_content        TEXT NOT NULL,
-     created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-     category_item_id    INTEGER,
-
-     CONSTRAINT fk_chunks_file
-         FOREIGN KEY (file_id) REFERENCES files(id),
-
-     CONSTRAINT uq_chunks_file_index
-         UNIQUE (file_id, chunk_index)
-);
-
-CREATE TABLE summary_chunks (
     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    file_id             INTEGER NOT NULL,
-    chunk_id            INTEGER NOT NULL,
-    summary_text        TEXT NOT NULL,
-    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    filename TEXT       NOT NULL UNIQUE,
+    original_filename   TEXT,
+    size_bytes BIGINT,
+    file_creation_date  DATE,
+    uploaded            BOOLEAN DEFAULT FALSE,
+    uploaded_at         TIMESTAMP,
     category_item_id    INTEGER,
 
-    CONSTRAINT fk_summary_file
-        FOREIGN KEY (file_id) REFERENCES files(id),
-
-    CONSTRAINT fk_summary_chunk
-        FOREIGN KEY (chunk_id) REFERENCES text_chunks(id),
-
-    CONSTRAINT uq_summary_chunk
-        UNIQUE (chunk_id)
+    CONSTRAINT fk_category_item_id
+    FOREIGN KEY (category_item_id) REFERENCES category_items(id) ON DELETE SET NULL
 );
 
--- Generated from summary chunks, not raw text.
-CREATE TABLE flashcards (
+-- Generated from files, summary_generation worker input, and aggregate worker deletes them when it is finished
+CREATE TABLE text_chunks (
+    id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    file_id             INTEGER NOT NULL, -- Workers gets this from StartDTO
+    chunk_index         INTEGER NOT NULL, -- Calculated by the worker (optional)
+    page_start          INTEGER, -- Calculated by the worker (optional)
+    page_end            INTEGER, -- Calculated by the worker (optional)
+    text_content        TEXT NOT NULL, -- Calculated by the worker (optional)
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    category_item_id    INTEGER, -- Workers gets this from StartDTO
+
+    CONSTRAINT fk_chunks_file
+        FOREIGN KEY (file_id) REFERENCES files(id),
+
+    CONSTRAINT uq_chunks_file_index
+        UNIQUE (file_id, chunk_index),
+        
+    CONSTRAINT fk_category_item_id
+        FOREIGN KEY (category_item_id) REFERENCES category_items(id) ON DELETE SET NULL
+);
+
+-- Generated from text chunks, flashcard_generation worker input, and aggregate worker deletes them when it is finished
+CREATE TABLE summary_chunks (
+    id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    chunk_id            INTEGER NOT NULL UNIQUE,
+    summary_text        TEXT NOT NULL,
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_summary_chunk -- Link back to the original text chunk (file, page, chunk_index info)
+        FOREIGN KEY (chunk_id) REFERENCES text_chunks(id)
+);
+
+-- Generated from summary chunks, aggregate worker input and  deletes them when it is finished
+CREATE TABLE temporary_flashcards (
+    id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    summary_chunk_id    INTEGER NOT NULL,
+    question            TEXT NOT NULL,
+    answer              TEXT NOT NULL,
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_flashcard_summary -- Link back to the original summary chunk (file, page, chunk_index info)
+        FOREIGN KEY (summary_chunk_id) REFERENCES summary_chunks(id)
+);
+
+-- Created from temporary flashcards by aggregate worker, linked to files and category items
+CREATE TABLE final_flashcards (
     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     file_id             INTEGER NOT NULL,
-    summary_chunk_id    INTEGER NOT NULL,
     question            TEXT NOT NULL,
     answer              TEXT NOT NULL,
     created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     category_item_id    INTEGER,
 
-    CONSTRAINT fk_flashcard_file
+    CONSTRAINT fk_final_flashcard_file
         FOREIGN KEY (file_id) REFERENCES files(id),
-
-    CONSTRAINT fk_flashcard_summary
-        FOREIGN KEY (summary_chunk_id) REFERENCES summary_chunks(id)
+        
+    CONSTRAINT fk_category_item_id
+        FOREIGN KEY (category_item_id) REFERENCES category_items(id) ON DELETE SET NULL
 );
 
+-- Created from summary chunks by aggregate worker, linked to files and category items
 CREATE TABLE final_summaries (
     file_id             INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     summary_text        TEXT NOT NULL,
@@ -107,22 +116,28 @@ CREATE TABLE final_summaries (
     category_item_id    INTEGER,
 
     CONSTRAINT fk_final_summary_file
-     FOREIGN KEY (file_id) REFERENCES files(id)
+        FOREIGN KEY (file_id) REFERENCES files(id),
+        
+    CONSTRAINT fk_category_item_id
+        FOREIGN KEY (category_item_id) REFERENCES category_items(id) ON DELETE SET NULL
 );
 
 CREATE TABLE jobs (
     id                  INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     file_id             INTEGER NOT NULL,
-    job_type            VARCHAR(50) NOT NULL,
-    status              VARCHAR(30) NOT NULL,
-    total_tasks         INTEGER DEFAULT 0 NOT NULL,
-    completed_tasks     INTEGER DEFAULT 0 NOT NULL,
+    job_type            VARCHAR(30) CHECK (job_type in ('TEXT_EXTRACTION', 'SUMMARY_GENERATION', 'FLASHCARD_GENERATION', 'AGGREGATION')) NOT NULL,
+    status              VARCHAR(30) CHECK (status in ('PENDING', 'IN_PROGRESS', 'FINISHED', 'FAILED')) NOT NULL,
+    --total_tasks         INTEGER DEFAULT 0 NOT NULL,
+    --completed_tasks     INTEGER DEFAULT 0 NOT NULL,
     created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     category_item_id    INTEGER,
 
     CONSTRAINT fk_jobs_file
-      FOREIGN KEY (file_id) REFERENCES files(id)
+        FOREIGN KEY (file_id) REFERENCES files(id),
+        
+    CONSTRAINT fk_category_item_id
+        FOREIGN KEY (category_item_id) REFERENCES category_items(id) ON DELETE SET NULL
 );
 
 CREATE INDEX idx_chunks_file ON text_chunks(file_id);
